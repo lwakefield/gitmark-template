1ed161f9a7f4c9e68c7630ab7772d4a48c7bbdcd
Lawrence Wakefield <lawrence@iamlawrence.me>
2020-07-11 10:19:54 -0400

    Fix month serialization

--- .github/workflows/render.js
+++ .github/workflows/render.js
@@ -55,7 +55,7 @@ const bookmarksGroupedByDayEntries = Object.entries(bookmarksGroupedByDay)
 for (const [ _date, bookmarks ] of bookmarksGroupedByDayEntries) {
     const date = new Date(_date);
     const year = date.getFullYear();
-    const month = date.getMonth().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
     readme += `\n## ${year}-${month}-${day}\n`;
     for (const bookmark of bookmarks) {
